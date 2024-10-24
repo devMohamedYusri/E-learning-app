8fcf20463d8f5013977b697f5324f24de6854c6b
Mohamed Yusri <mohamedyousry373,d@gmail.com>
2024-10-24 04:13:03 +0300

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Home from "./componants/home/Home";
 
-import InstructorDashboard from "./pages/InstructorDashboard"; // Instructor Courses Page
-import AddEditCourse from "./pages/AddEditCourse"; // Add/Edit Courses page
-import SignUp from "./componants/sign/SignUp";
-import InstructorSign from "./componants//sign/instructorSign";
-import Login from "./componants/LoginPage/Login";
+import InstructorDashboard from "./pages/InstructorDashboard.jsx";
+import AddEditCourse from "./pages/AddEditCourse.jsx"; 
+import SignUp from "./componants/sign/SignUp.jsx";
+import InstructorSign from "./componants//sign/instructorSign.jsx";
+import Login from "./componants/LoginPage/login.jsx";
 import Courses from "./componants/courses/courses.jsx"; 
 import Contact from "./componants/contact/Contactus"; 
 import Footer from "./componants/footer/Footer.jsx";
@@ -14,25 +14,20 @@ import Details from "./componants/CourseInfoDetails/Details.jsx";
 import MyCourses from "./componants/Mycourses/MyCourses.jsx";
 
 export default function App() {
-  // Saving Courses data
   const [courses, setCourses] = useState([]);
 
-  // Delete A Course Function
   const handleDeleteCourse = (courseId) => {
     setCourses(courses.filter((course) => course.id !== courseId));
   };
 
-  // Add/Edit Course Function
   const handleSaveCourse = (newCourseData) => {
     if (newCourseData.id) {
-      // Edit Course
       setCourses(
         courses.map((course) =>
           course.id === newCourseData.id ? newCourseData : course
         )
       );
     } else {
-      // Add A New Course
       setCourses([...courses, { ...newCourseData, id: courses.length + 1 }]);
     }
   };
--- src/componants/CourseInfoDetails/Details.jsx
+++ src/componants/CourseInfoDetails/Details.jsx
@@ -89,24 +89,4 @@ function Details() {
         </div>
     );
 }
-
-const StarRating = ({ rating }) => {
-    return (
-        <>
-            {Array(5)
-                .fill()
-                .map((_, index) => (
-                    <svg
-                        key={index}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill={index < rating ? "orange" : "gray"}
-                        className="w-6 h-6">
-                        <path d="M12 17.27L18.18 21 16.54 13.97 22 9.24 14.81 8.63 12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-                    </svg>
-                ))}
-        </>
-    );
-};
-
 export default Details;
